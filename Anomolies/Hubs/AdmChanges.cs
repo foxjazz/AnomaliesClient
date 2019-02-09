11d05a27a0c057599a200fd89510e14211edb2b7
mordagel <mordagel@outlook.com>
2019-02-09 06:59:06 -0700

    Scratch

---
+++ Anomolies/Hubs/AdmChanges.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Anomalies.Model;
+using Anomalies.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace Anomalies.Hubs
+{
+
+    //  [HubName("admchanges")]
+    public class AdmChanges : Hub
+    {
+        private Repo _repo;
+        public AdmChanges(Repo repo)
+        {
+            _repo = repo;
+        }
+        public Task AddAdm( string name, int system)
+        {
+            
+            var repo = _repo;
+            
+            var eveHome = repo.ReadDy();
+            for (int i = 0; i < eveHome.eveSystem.Count; i++)
+            {
+                if (eveHome.eveSystem[i].id == system)
+                {
+                    eveHome.eveSystem[i].adms.Add(new Adm { id = 1, name = name });
+                }
+            }
+            repo.Save( eveHome.ToString(), eveHome);
+            return Clients.All.SendAsync("AddAdm", name, system);
+        }
+        public Task RemoveAdm(string name, int system)
+        {
+            
+            var repo = _repo;
+            var eveHome = repo.ReadDy();
+            for (int i = 0; i < eveHome.eveSystem.Count; i++)
+            {
+                if (eveHome.eveSystem[i].id == system)
+                {
+                    for (int ii = 0; ii < eveHome.eveSystem[i].adms.Count; ii++)
+                    {
+                        var adms = eveHome.eveSystem[i].adms as List<Adm>;
+                        var adm = adms.FirstOrDefault(n => n.name == name);
+                        adms.Remove(adm);
+                    }
+                    eveHome.eveSystem[i].adms.Add(new Adm { id = 1, name = name });
+                }
+            }
+            repo.Save(eveHome.ToString(), eveHome);
+            return Clients.All.SendAsync("RemoveAdm", name, system);
+
+        }
+
+    }
+}
--- Anomolies/Startup.cs
+++ Anomolies/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Anomalies.Hubs;
+using Anomalies.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,7 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Anomolies
+namespace Anomalies
 {
     public class Startup
     {
@@ -25,6 +27,11 @@ namespace Anomolies
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddOptions();
+
+            services.AddSingleton<Repo>();
+            services.AddSignalR();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,7 +47,11 @@ namespace Anomolies
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(config =>
+                config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseHttpsRedirection();
+            app.UseSignalR(routes => { routes.MapHub<ChatMessages>("/chatmessages"); });
+            app.UseSignalR(routes => { routes.MapHub<AdmChanges>("/admchanges"); });
             app.UseHttpsRedirection();
             app.UseMvc();
         }
